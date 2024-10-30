9538e48e992c9b785cd7236971a72e27490238aa
surfsky <surfsky@sina.com>
2024-10-30 18:10:03 +0800

    toast, img.icon

--- Task.md
+++ Task.md
@@ -1,21 +1,32 @@
 ------------------------------------------------------
 task
 ------------------------------------------------------
-click传递的this有问题，见 animate.html
-gridcolumn
-click, asyncClick 统一处理。
-<StyleInit>
-<icon><icona>
-实现 panel
-实现各种 popup
+bug
+    click传递的this有问题，见 animate.html
+    gridcolumn 有问题
+    
+迁移
+    实现 StyleInit
+    实现 icon
+    实现 icona
+
+实现复杂控件
+    实现 panel
+    实现 switch
+    动态创建的对象中的子对象解析有问题，如<toast.row>
+    实现各种 popup
 
 
-用 nocss 实现一个控件，如switch、panel
+剥离扩展控件属性的逻辑，需要给标签附上一个onAttributeChanged(name, newValue)方法
+
 
 
 ------------------------------------------------------
 done
 ------------------------------------------------------
+/img.icon
+/click 统一处理。
+/toast
 /把方法注入到 HTMLElement 下，简化代码（算了）
 /创建一个框架，支持自定义标签，不用类似 x-mytag 这样的格式
     /Container
--- nocss.js
+++ nocss.js
@@ -7,6 +7,7 @@ const Anchor = {
     TR: 'topRight',
     L:  'left',
     C:  'center',
+    CH: 'centerH',
     R:  'right',
     BL: 'bottomLeft',
     B:  'bottom',
@@ -462,9 +463,11 @@ class NoCss{
                         });
                     }
                 }
-                //else if (mutation.type === 'attributes') {
-                //    this.setCustomAttribute(ele, mutation.attributeName, ele.getAttribute(mutation.attributeName));
-                //}
+                else if (mutation.type === 'attributes') {
+                    var name = mutation.attributeName;
+                    if (name != 'style' && name != 'id')
+                        this.setCustomAttribute(ele, name, ele.getAttribute(name));
+                }
             }
         });
         observer.observe(document, { attributes: true, childList: true, subtree: true });
@@ -771,6 +774,7 @@ class NoCss{
                 case Anchor.TR  : s.flexDirection='row';     s.justifyContent='flex-end';    s.alignItems='flex-start'; break;
                 case Anchor.L   : s.flexDirection='row';     s.justifyContent='flex-start';  s.alignItems='center';     break;
                 case Anchor.C   : s.flexDirection='column';  s.justifyContent='center';      s.alignItems='center';     break;
+                case Anchor.CH  : s.flexDirection='row';     s.justifyContent='center';      s.alignItems='center';     break;
                 case Anchor.R   : s.flexDirection='row';     s.justifyContent='flex-end';    s.alignItems='center';     break;
                 case Anchor.BL  : s.flexDirection='row';     s.justifyContent='flex-start';  s.alignItems='flex-end';   break;
                 case Anchor.B   : s.flexDirection='row';     s.justifyContent='center';      s.alignItems='flex-end';   break;
@@ -1234,14 +1238,17 @@ class Button extends Tag {
         NoCss.setHoverOpacity(ele, '0.8');
 
         // event
-        var click = ele.getAttribute('click');
-        if (click != null) this.setClick(ele, click, false);
+        //var click = ele.getAttribute('click');
+        //if (click != null) this.setClick(ele, click, false);
         var asyncClick = ele.getAttribute('asyncClick');
         if (asyncClick != null) this.setClick(ele, asyncClick, true);
 
         // theme
         ele.setTheme = function(){
+            var themeCls = ele.getAttribute('themeCls');
+            if (themeCls == null) themeCls = 'primary';
             Theme.setBaseTheme(ele);
+
             var o = Theme.current;
             ele.style.borderRadius = o.radius;
             ele.style.color = o.textLight;
@@ -1289,5 +1296,186 @@ class Button extends Tag {
                 //this.setEnable(true);
             });
         }
+    }
+}
+
+
+/************************************************************
+ * Mask
+ * @example
+ *     Mask.show(100);
+ *     Mask.hide();
+ ***********************************************************/
+class Mask {
+    static async show(z = 99) {
+        if (this.overlay == null) {
+            this.overlay = document.createElement('div');
+            this.overlay.style.position = 'fixed';
+            this.overlay.style.top = 0;
+            this.overlay.style.left = 0;
+            this.overlay.style.width = '100%';
+            this.overlay.style.height = '100%';
+            this.overlay.style.display = 'none';
+            this.overlay.style.transition = 'all 0.5s';
+            this.overlay.style.zIndex = z;
+            document.body.appendChild(this.overlay);
+        }
+        this.overlay.style.display = 'block';
+        //this.overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.0)';
+        await Utils.sleep(50);
+        this.overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
+    }
+
+    static async hide() {
+        if (this.overlay != null) {
+            this.overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.0)';
+            await Utils.sleep(500);
+            this.overlay.style.display = 'none';
+            document.body.removeChild(this.overlay);
+            this.overlay = null;
+        }
+    }
+}
+
+
+
+/************************************************************
+ * Toast
+ * @example
+ *     Toast.show('info', 'message info');
+ ***********************************************************/
+class Toast {
+    static counter = 0;
+    /**
+     * Show toast
+     * @param {string} icon iconname without extension
+     * @param {string} text information 
+     */
+    static async show(text, icon='white-bulb', width='400px', height='38px') {
+        var id = Utils.uuid();
+        var tag = `
+            <rect id='${id}' box='border-box' fixanchor='top' top='-100px' childanchor='centerH'
+              width='${width}' height='${height}' radius='6px'  border='0'
+              bgcolor='${Theme.current.success}' color='${Theme.current.light}' opacity='0.8'>
+                <img src='${Utils.getIconUrl(icon)}' width='20px' height='20px'/>&nbsp;
+                <div>${text}<div>
+            </rect>
+        `;
+
+        // add to body
+        var ele = Utils.parseElement(tag);
+        document.body.appendChild(ele);
+
+        // parse height's value and unit, calc top position.
+        this.counter++;
+        const regex = /(\d+(?:\.\d+)?)(px|rem|em|%)/;
+        const match = height.match(regex);
+        const value = match ? parseFloat(match[1]) : 38;
+        const unit  = match ? match[2] : 'px';
+        var top = 25 + (this.counter-1)*(value+10) + unit;
+
+        // show and hide with animation
+        var toast = Utils.$('#' + id);  // = ele.root
+        await Utils.sleep(50);
+        toast.style.top = top;
+        await Utils.sleep(2000);
+        toast.style.top = '-100px';
+        await Utils.sleep(1000);
+        document.body.removeChild(toast);
+        this.counter--;
+    }
+
+}
+
+/************************************************************
+ * Tooltip
+ * @example
+ *     Tooltip.show(ele, 'message info');
+ *     Tooltip.bind('#id');
+ *     Tooltip.hide();
+ ***********************************************************/
+class Tooltip {
+    /** Bind all matched elements to show tooltip
+     * @param {string} selector Element selector 
+     * @param {string} [attrName='tip'] Attribute name or callback， If null, show element's text content.
+    */
+    static bind(selector, attrName=null) {
+        const elements = document.querySelectorAll(selector);
+        elements.forEach(ele => {
+            var o = (ele.root == null) ? ele : ele.root;
+            o.addEventListener('mouseover', () => Tooltip.show(ele, attrName));
+            o.addEventListener('mouseout',  () => Tooltip.hide());
+        });
+    }
+
+    /**
+     * Show tooltip under element. 
+     * @param {Tag} element 
+     * @param {string} attrName
+     */
+    static show(element, attrName) {
+        var text = '';
+        if (attrName == null)                    text = element.textContent;
+        else if (typeof attrName == 'function')  text = attrName(element);
+        else                                     text = this.getVal(element, attrName);
+        if (text == null || text == '')
+            return;
+
+        const tooltip = document.createElement('div');
+        tooltip.id = 'tooltip';
+        tooltip.innerHTML = text;
+        tooltip.style.display = 'block';
+        tooltip.style.position = "fixed";
+        tooltip.style.backgroundColor = 'white'; //"#f9f9f9";
+        tooltip.style.border = "1px solid #ccc";
+        tooltip.style.borderRadius = '4px';
+        tooltip.style.padding = "5px";
+        tooltip.style.zIndex = "999";
+
+        var rect = element.getBoundingClientRect(); // get rect in viewport
+        tooltip.style.left = rect.left + 'px';
+        tooltip.style.top  = rect.bottom + 4 + 'px';
+        document.body.appendChild(tooltip);
+    }
+
+    /** Hide tooltip */
+    static hide() {
+        const tooltip = document.getElementById('tooltip');
+        if (tooltip != null)
+            document.body.removeChild(tooltip);
+    }
+
+    /** Get element's attribute's value 
+     * @param {Element} element 
+     * @param {string} attr Attribute name, can be comma-seperated string: 'style.width'
+    */
+    static getVal(element, attr) {
+        let attrs = attr.split('.');
+        let value = element;
+        for (let a of attrs) {
+            value = value[a];
+            if (value === undefined) {
+                return undefined;
+            }
+        }
+        return value;
+    }
+}
+
+
+/***********************************************************
+ * Column container
+ * @example
+ *     <img icon="20px">
+ ***********************************************************/
+class Image extends Tag {
+    static { NoCss.registCustomTag('Img', new Image());}
+
+    /**@param {HTMLElement}} ele */
+    render(ele) {
+        var icon = ele.getAttribute('icon');
+        if (icon != null)
+            ele.src = `${Utils.getIconUrl(icon)}`;
+        return ele;
     }
 }
